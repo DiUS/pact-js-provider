1561baf3e4aefb89e904174065cada2fa70fa779
Elliott Murray <emurray@dius.com.au>
2016-01-21 11:06:39 +1100

    es6

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-require('babel-core/register');
-
 var Pact = require('./lib/pact.js')
-// import Pact from './src/pact.js';
 
 module.exports = Pact;
---
+++ index_es6.js
@@ -0,0 +1,6 @@
+require('babel-core/register');
+
+var Pact = require('./src/pact.js')
+// import Pact from './src/pact.js';
+
+module.exports = Pact;
